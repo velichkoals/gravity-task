7a5da8e0caee738f12f1abfca68018339addc222
ALEXEY <velichkoalsprog@gmail.com>
2022-08-30 10:08:34 +0300

    feat: add routes, scss variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
+import { HomePage } from './pages/HomePage/HomePage';
 
 import './App.scss';
 
 function App() {
-	return <div className='App'>Gravity Task</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='*' element={<NotFoundPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
